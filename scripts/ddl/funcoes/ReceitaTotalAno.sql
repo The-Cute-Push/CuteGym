d7ff60e6c5e18c0fc76d6a75e5f5f45dfff0efe0
Vitor Muneretto Tinelli <vitor.mtinelli@gmail.com>
2025-06-22 18:06:45 -0300

    feat: function that will be used in question

---
+++ scripts/ddl/funcoes/ReceitaTotalAno.sql
@@ -0,0 +1,19 @@
+DROP FUNCTION IF EXISTS ReceitaTotalAno
+GO
+
+CREATE FUNCTION ReceitaTotalAno(@Plano NVARCHAR(100), @Ano INT)
+RETURNS NUMERIC(18, 2)
+AS
+BEGIN
+    DECLARE @ReceitaTotal NUMERIC(18, 2);
+
+    SELECT @ReceitaTotal = COALESCE(SUM(PA.price), 0)
+    FROM plans P
+    JOIN subscriptions S ON P.id_plan = S.id_plan
+    JOIN payments PA ON S.id_subscriptions = PA.id_subscriptions
+    WHERE P.[name] = @Plano
+      AND YEAR(S.dt_start) = @Ano
+      AND S.active = 1;
+
+    RETURN @ReceitaTotal;
+END;
