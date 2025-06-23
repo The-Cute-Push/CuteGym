e6a39167fef141d71af704fcea50bc9072d7f4b3
Lucas De Bitencourt Frasson <lucasb.frasson@gmail.com>
2025-06-23 11:27:52 -0300

    Optimizei o código para utilizar 2 CTE ao invés de fazer duas vezes o mesmo cálculo

--- scripts/dql/lucas_question/answer.sql
+++ scripts/dql/lucas_question/answer.sql
@@ -1,43 +1,42 @@
-WITH UsersLeais AS (
+WITH CalculoDias AS (
     SELECT
         U.id_user,
         U.nm_user,
         U.email,
-        S.dt_start,
         P.name AS NomePlano,
-        P.duration_mnth,
-        S.active,
         DATEDIFF(day, S.dt_start,
             CASE
-                WHEN S.active = 1 THEN GETDATE() -- Active é BIT, logo, 1 = TRUE
-                -- Se inativa, a data final é a dt_start + duration_mnth, mas apenas se for no passado
-                WHEN S.active = 0 AND DATEADD(month, P.duration_mnth, S.dt_start) < GETDATE() THEN DATEADD(month, P.duration_mnth, S.dt_start)
-                ELSE S.dt_start -- Caso a assinatura inativa tenha terminado no futuro, ou seja, um caso atípico
-            END
-        ) AS DiasAtivo,
-        ROW_NUMBER() OVER (ORDER BY
-            DATEDIFF(day, S.dt_start,
-                CASE
-                    WHEN S.active = 1 THEN GETDATE()
-                    WHEN S.active = 0 AND DATEADD(month, P.duration_mnth, S.dt_start) < GETDATE() THEN DATEADD(month, P.duration_mnth, S.dt_start)
-                    ELSE S.dt_start
-                END
-            ) DESC
-        ) AS RNK
+                -- Se ainda está assinando na nossa academia
+                WHEN S.active = 1 THEN GETDATE() -- active é BIT, logo 1 = TRUE
+                -- Se ainda não está mais assinando na nossa academia e terminou hoje ou no passado
+                WHEN S.active = 0 AND DATEADD(month, P.duration_mnth, S.dt_start) <= GETDATE() THEN DATEADD(month, P.duration_mnth, S.dt_start)
+                -- Se terminou a assinatura no futuro, algo que não deveria ter como
+                ELSE S.dt_start
+            END9
+        ) AS DiasAtivo
     FROM users U
     JOIN user_types UT ON U.id_user_type = UT.id_user_type
     JOIN subscriptions S ON U.id_user = S.id_user
     JOIN plans P ON S.id_plan = P.id_plan
     WHERE UT.name = 'Member'
+),
+RankingLealdade AS (
+    SELECT
+        nm_user,
+        email,
+        NomePlano,
+        DiasAtivo,
+        ROW_NUMBER() OVER (ORDER BY DiasAtivo DESC) AS RNK
+    FROM CalculoDias
 )
 SELECT
-    UL.nm_user,
-    UL.email,
-    UL.NomePlano,
-    UL.DiasAtivo
-FROM UsersLeais UL
-WHERE UL.RNK <= 5
-ORDER BY UL.DiasAtivo DESC;
+    nm_user,
+    email,
+    NomePlano,
+    DiasAtivo
+FROM RankingLealdade
+WHERE RNK <= 5
+ORDER BY DiasAtivo DESC;
 go
 
--- Now add the index and the select again in the same script with execution plan active to see the difference
+-- Agora adicionar os índices ddl/indices/idx_pergunta_lucas.sql e repetir o select para comparar no plano de execução com e sem os índices.
